a237bc3ac90e72151b2461bb4b33e2ecc3c231bd
parinaz mirbagheri <parinazmirbagheri@gmail.com>
2020-11-23 09:26:17 -0800

    Configure animations

---
+++ Assets/Scripts/CollisionBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour
+{
+    public bool collided;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collided)
+        {
+            collided = true;
+
+            GameObject clown = gameObject.transform.parent.gameObject;
+            Animator clownAnimator = clown.GetComponent<Animator>();
+
+            clownAnimator.SetTrigger("Hit");
+        }
+    }
+}
